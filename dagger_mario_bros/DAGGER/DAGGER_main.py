d094a608b0be09d3f6315715cc5350a59ecb6e0e
ManyaZ1 <96390140+ManyaZ1@users.noreply.github.com>
2025-06-09 19:04:29 +0300

    stuck gym

--- dagger_mario_bros/DAGGER/DAGGER_main.py
+++ dagger_mario_bros/DAGGER/DAGGER_main.py
@@ -17,8 +17,9 @@ def main():
     trainer = MarioTrainer(world = '1', stage = '1', action_type = 'simple')
 
     # 'mario_FLAG_iter474_ep5_3394_20250609_053224.pth' καθεται διπλα στη σημαια και αραζει
+    #'mario_FLAG_iter192_ep12_3416_20250609_182914.pth'σκαλα
     model_path = os.path.join( 
-        base_dir, 'SUCCESS', 'mario_FLAG_iter574_ep19_3425_20250609_072905-success.pth'
+        base_dir, 'models_dagger', 'dagger_mario_iter200_20250609_184043.pth'#'mario_FLAG_iter574_ep19_3425_20250609_072905-success.pth'
         #'mario_FLAG_iter105_ep12_3346_20250608_154904.pth'
     )
 
